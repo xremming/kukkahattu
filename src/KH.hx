47364f30afe0383aee799edf3d530dfb62945627
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-01-16 20:42:54 +0200

    Fixed merge

--- src/KH.hx
+++ src/KH.hx
@@ -2,7 +2,6 @@ class KH
 {
 	public static var score:Int = 0;
 	public static var highScore:Int = 0;
-<<<<<<< HEAD
 
 	public static var playerFireRate:Float = 3;
 
@@ -10,5 +9,4 @@ class KH
 
 	public static var spawnRate:Float = 0.5;
 	public static var spawnDeviation:Float = 5;
->>>>>>> e9f4c67d4a9009d77fdfff45755c45a745ebf3c8
 }
