e9f4c67d4a9009d77fdfff45755c45a745ebf3c8
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-01-16 20:38:32 +0200

    recipe droprate and nazi spawn rate moved to KH

--- src/KH.hx
+++ src/KH.hx
@@ -3,4 +3,6 @@ class KH
 	public static var score:Int = 0;
 	public static var highScore:Int = 0;
 	public static var recipeDroprate:Float = 0.7;
+	public static var spawnRate:Float = 0.5;
+	public static var spawnDeviation:Float = 5;
 }
